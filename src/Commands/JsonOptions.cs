326f13db468d95f5f724d23773be829445321cb5
Daniel Cazzulino <daniel@cazzulino.com>
2023-08-14 19:01:17 -0300

    Apply oss template, cleanup paths, refactor code
    Move the actual behavior to a separate project, in case we end up also shipping a dotnet global tool (for example).

---
+++ src/Commands/JsonOptions.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace Devlooped.SponsorLink;
+
+static class JsonOptions
+{
+    public static JsonSerializerOptions Default { get; } = new(JsonSerializerDefaults.Web)
+    {
+        AllowTrailingCommas = true,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        ReadCommentHandling = JsonCommentHandling.Skip,
+        WriteIndented = true,
+    };
+}
---
+++ src/Commands/ShowCommand.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using Spectre.Console;
+using Spectre.Console.Cli;
+using Spectre.Console.Json;
+
+namespace Devlooped.SponsorLink;
+
+public class ShowCommand : Command
+{
+    public override int Execute(CommandContext context)
+    {
+        if (!GitHub.IsInstalled)
+        {
+            AnsiConsole.MarkupLine("[yellow]Please install GitHub CLI from [/][link]https://cli.github.com/[/]");
+            return -1;
+        }
+
+        if (GitHub.Authenticate() is not { } account)
+        {
+            AnsiConsole.MarkupLine("Please run [yellow]gh auth login[/] to authenticate, [yellow]gh auth status -h github.com[/] to verify your status.");
+            return -1;
+        }
+
+        var json = JsonSerializer.Serialize(account, JsonOptions.Default);
+        AnsiConsole.Write(
+            new Panel(new JsonText(json))
+                .Header("Authenticated")
+                .Collapse()
+                .RoundedBorder()
+                .BorderColor(Color.Green));
+
+        //AnsiConsole.MarkupLine($"[green]Authenticated as {account.Login}[/] [grey](id={account.Id})[/]");
+
+        return 0;
+    }
+}
