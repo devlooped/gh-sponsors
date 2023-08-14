5d389ec5f8381a74a3a3614dc3b64c636514b487
Daniel Cazzulino <daniel@cazzulino.com>
2023-08-14 13:03:20 -0300

    Testing out a gh extension

---
+++ GitHub.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Devlooped.SponsorLink;
+
+public record Account([property: JsonPropertyName("node_id")] string Id, string Login);
+
+public static class GitHub
+{
+    public static bool IsInstalled { get; } = TryIsInstalled(out var _);
+
+    public static bool TryIsInstalled(out string output)
+        => Process.TryExecute("gh", "--version", out output) && output.StartsWith("gh version");
+
+    //public static bool TryApi(string endpoint, out JToken? json)
+    //{
+    //    json = null;
+
+    //    return Process.TryExecute("gh", "api " + endpoint, out var data) &&
+    //        (json = JsonConvert.DeserializeObject<JToken>(data)) != null;
+    //}
+
+    public static Account? Authenticate()
+    {
+        if (!Process.TryExecute("gh", "auth status -h github.com", out var output))
+            return default;
+
+        if (output.Contains("gh auth login"))
+            return default;
+
+        if (!Process.TryExecute("gh", "api user", out output))
+            return default;
+
+        return JsonSerializer.Deserialize<Account>(output, new JsonSerializerOptions(JsonSerializerDefaults.Web));
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Devlooped.SponsorLink;
+using Spectre.Console;
+
+if (!GitHub.IsInstalled)
+{
+    AnsiConsole.MarkupLine("[yellow]Please install GitHub CLI from [/][link]https://cli.github.com/[/]");
+    return -1;
+}
+
+if (GitHub.Authenticate() is not { } account)
+{
+    AnsiConsole.MarkupLine("Please run [yellow]gh auth login[/] to authenticate, [yellow]gh auth status -h github.com[/] to verify your status.");
+    return -1;
+}
+
+AnsiConsole.MarkupLine($"[green]Authenticated as {account.Login}[/] [grey](id={account.Id})[/]");
+
+return 0;
