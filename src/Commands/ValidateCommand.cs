a3db676892f04afb5b61ae8abf26c00cef167cfd
Daniel Cazzulino <daniel@cazzulino.com>
2023-08-23 12:58:35 -0300

    In addition to the hashing, which might still be concerning since knowing your sponsors' emails and sponsored accounts might still allow reconstructing the hash, adding a random, per-install GUID completely removes this possibility.
    The new Session handles these environment variables so we don't even incur any I/O down the road from the analyzer:
    
    * SPONSORLINK_INSTALLATION: a GUID created if not already present (can be cleared by the user at any time to completely change all future hashes as needed), used for salting all hashes.
    * SPONSORLINK_TOKEN: an access token used to invoke the SponsorLink API to sign the manifest hashes. This is done only to allow integrity verification at analyzer/check time.
    * SPONSORLINK_MANIFEST: last sync'ed manifest JWT to check for sponsorships.
    
    Since the hashes are now effectively opaque by the server, all the server would do is sign the JWT received in the `/sign` endpoint with the corresponding private key, but otherwise, the JWT remains intact (only the expiration date is set from the server-side too when signing).
    
    Related to https://github.com/devlooped/SponsorLink/issues/31

--- src/Commands/ValidateCommand.cs
+++ src/Commands/ValidateCommand.cs
@@ -4,34 +4,34 @@ using Spectre.Console.Cli;
 
 namespace Devlooped.SponsorLink;
 
-public partial class ValidateCommand : GitHubCommand
+public partial class ValidateCommand : Command
 {
-    protected override int OnExecute(Account user, CommandContext context)
+    public override int Execute(CommandContext context)
     {
-        var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), ".sponsorlink");
-        if (!File.Exists(path))
+        var token = Environment.GetEnvironmentVariable("SPONSORLINK_MANIFEST", EnvironmentVariableTarget.User);
+        if (string.IsNullOrEmpty(token))
         {
-            AnsiConsole.MarkupLine("[red]No SponsorLink file found in your home directory.[/] Run [white]gh sponsors link[/] to initialize it.");
+            AnsiConsole.MarkupLine("[red]No SponsorLink manifest found.[/] Run [white]gh sponsors sync[/] to initialize it.");
             return -1;
         }
 
         try
         {
-            Manifest.Read(File.ReadAllText(path));
+            Manifest.Read(token);
         }
         catch (SecurityTokenExpiredException)
         {
-            AnsiConsole.MarkupLine("[red]The manifest has expired.[/] Run [white]gh sponsors link[/] to generate a new one.");
+            AnsiConsole.MarkupLine("[red]The manifest has expired.[/] Run [white]gh sponsors sync[/] to generate a new one.");
             return -2;
         }
         catch (SecurityTokenInvalidSignatureException)
         {
-            AnsiConsole.MarkupLine("[red]The manifest signature is invalid.[/] Run [white]gh sponsors link[/] to generate a new one.");
+            AnsiConsole.MarkupLine("[red]The manifest signature is invalid.[/] Run [white]gh sponsors sync[/] to generate a new one.");
             return -3;
         }
-        catch (Exception ex)
+        catch (SecurityTokenException ex)
         {
-            AnsiConsole.MarkupLine($"[red]The manifest is invalid.[/] Run [white]gh sponsors link[/] to generate a new one.");
+            AnsiConsole.MarkupLine($"[red]The manifest is invalid.[/] Run [white]gh sponsors sync[/] to generate a new one.");
             AnsiConsole.WriteException(ex);
             return -4;
         }
--- src/Extension/Program.cs
+++ src/Extension/Program.cs
@@ -1,14 +1,32 @@
 ﻿using Devlooped.SponsorLink;
+using Microsoft.Extensions.DependencyInjection;
 using Spectre.Console;
 using Spectre.Console.Cli;
 
-var app = new CommandApp();
+if (!GitHub.IsInstalled)
+{
+    AnsiConsole.MarkupLine("[yellow]Please install GitHub CLI from [/][link]https://cli.github.com/[/]");
+    return -1;
+}
+
+if (GitHub.Authenticate() is not { } account)
+{
+    AnsiConsole.MarkupLine("Please run [yellow]gh auth login[/] to authenticate, [yellow]gh auth status -h github.com[/] to verify your status.");
+    return -1;
+}
+
+// Provide the authenticated GH CLI user account via DI
+var registrations = new ServiceCollection();
+registrations.AddSingleton(account);
+var registrar = new TypeRegistrar(registrations);
+
+var app = new CommandApp<SyncCommand>(registrar);
 app.Configure(config =>
 {
-    config.AddCommand<ShowCommand>();
-    config.AddCommand<ListCommand>();
-    config.AddCommand<LinkCommand>();
-    config.AddCommand<ValidateCommand>();
+    //config.AddCommand<ShowCommand>();
+    config.AddCommand<ListCommand>().WithDescription("Lists user and organization sponsorships");
+    config.AddCommand<SyncCommand>().WithDescription("Synchronizes the sponsorships manifest");
+    config.AddCommand<ValidateCommand>().WithDescription("Validates the active sponsorships manifest, if any");
 
 #if DEBUG
     //config.PropagateExceptions();
@@ -16,6 +34,7 @@ app.Configure(config =>
 #endif
 });
 
+#if DEBUG
 if (args.Length == 0)
 {
     var command = AnsiConsole.Prompt(
@@ -23,10 +42,13 @@ if (args.Length == 0)
             .Title("Command to run:")
             .AddChoices(new[]
             {
-                "show", "list", "link", "validate"
+                "list",
+                "sync",
+                "validate"
             }));
 
     args = new[] { command };
 }
+#endif
 
 return app.Run(args);
