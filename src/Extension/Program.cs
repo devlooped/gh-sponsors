c93c5931e0dafc2f385698c7af10737d1301943d
Daniel Cazzulino <daniel@cazzulino.com>
2023-08-24 19:52:42 -0300

    If user scope is not available, we can't read emails
    In order for the hashes to contain the user's emails, the GH CLI needs to be authenticated with user scope. We missed this check before. Offer to do this automatically for the user just like we do login now.

--- src/Extension/Program.cs
+++ src/Extension/Program.cs
@@ -36,6 +36,31 @@ if (GitHub.Authenticate() is not { } account)
         return -1;
     }
 }
+else if (account.Emails.Length == 0)
+{
+    if (AnsiConsole.Confirm(ThisAssembly.Strings.GitHub.UserScope))
+    {
+        var process = Process.Start("gh", "auth refresh -h github.com -s user");
+        process.WaitForExit();
+        if (process.ExitCode != 0)
+            return process.ExitCode;
+
+        // Re-authenticate to read emails again.
+        account = GitHub.Authenticate();
+        if (account is null)
+        {
+            AnsiConsole.MarkupLine("[red]x[/] Could not retrieve authenticated user with GitHub CLI.");
+            AnsiConsole.MarkupLine("[grey]-[/] Please run [yellow]gh auth login[/] to authenticate, [yellow]gh auth status -h github.com[/] to verify your status.");
+            return -1;
+        }
+    }
+    else
+    {
+        AnsiConsole.MarkupLine("[red]x[/] Could not retrieve authenticated user's email(s). This is required to sync your sponsorship manifest.");
+        AnsiConsole.MarkupLine("[grey]-[/] Please run [yellow]gh auth refresh - h github.com - s user[/] to enable 'user' scope and fix that manually.");
+        return -1;
+    }
+}
 
 // Provide the authenticated GH CLI user account via DI
 var registrations = new ServiceCollection();
